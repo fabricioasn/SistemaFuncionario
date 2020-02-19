e37b30c9f1d60859c0c3582c9bd9196f8a38a0b3
Fabricio <Fabricio@DESKTOP-9I1S7SI>
2020-02-19 17:52:13 -0300

    Sistema básico de cadastro de funcionários em banco SQL SERVER Utilizando JDBC para persistência.

--- src/sistema/telas/CargosConsultar.java
+++ src/sistema/telas/CargosConsultar.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sistema.telas;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JPanel;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.DefaultListModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import sistema.BancoDeDados;
+import sistema.entidades.Cargo;
 
 /**
  *tela para cosulta de dados na entidade cargos que busca a tablea cargod do BD
  * @author Fabricio Almeida da Silva Nunes
  */
-public class CargosConsultar {
+public class CargosConsultar extends JPanel {
+Cargo nowRole;  
+JLabel labelTitle, labelRole;
+JTextField fieldRole;
+JButton buttonSearch, buttonEdit, buttonDelete;
+DefaultListModel <Cargo> roleListModels = new DefaultListModel();
+JList <Cargo> listaCargos;   
+
+public CargosConsultar(){
     
+}
+
 }
