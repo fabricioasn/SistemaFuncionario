550ce63780d53c09f436528fc29fce55090ffec7
Fabricio <Fabricio@DESKTOP-9I1S7SI>
2020-02-20 03:44:45 -0300

    Sistema simples de cadastro de funcionários em Java conectando-se a um banco SQL SERVER usando JDBC

---
+++ src/sistema/Navegador.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema;
+
+/**
+ * Classe que gerencia a navegação pelo sistema entre todas as telas, administra o menu
+ * e comunica todos os Swing DAOs
+ * @author Fabricio Almeida da Silva Nunes
+ */
+public class Navegador {
+    
+}
